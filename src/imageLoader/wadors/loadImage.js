e06adc74eb4a7d0cf06c51a4f2a1f5b589279767
zzc <zhzc130@hotmail.com>
2018-09-20 17:11:16 +0800

    fix bugs

--- src/imageLoader/wadors/loadImage.js
+++ src/imageLoader/wadors/loadImage.js
@@ -8,66 +8,86 @@ import createImage from '../createImage.js';
  * @return The transfer-syntax as announced by the server, or Implicit Little Endian by default.
  */
 export function getTransferSyntaxForContentType (contentType) {
-  let transferSyntax = '1.2.840.10008.1.2'; // Default is Implicit Little Endian.
+    let transferSyntax = '1.2.840.10008.1.2'; // Default is Implicit Little Endian.
 
-  if (contentType) {
-    // Browse through the content type parameters
-    const parameters = contentType.split(';');
+    if (contentType) {
+        // Browse through the content type parameters
+        const parameters = contentType.split(';');
 
-    parameters.forEach((parameter) => {
-      // Look for a transfer-syntax=XXXX pair
-      const parameterValues = parameter.split('=');
+        parameters.forEach(parameter => {
+            // Look for a transfer-syntax=XXXX pair
+            const parameterValues = parameter.split('=');
 
-      if (parameterValues.length !== 2) {
-        return;
-      }
+            if (parameterValues.length !== 2) {
+                return;
+            }
 
-      if (parameterValues[0].trim() === 'transfer-syntax') {
-        transferSyntax = parameterValues[1].trim() || transferSyntax;
-      }
-    });
-  }
+            if (parameterValues[0].trim() === 'transfer-syntax') {
+                transferSyntax = parameterValues[1].trim() || transferSyntax;
+            }
+        });
+    }
 
-  return transferSyntax;
+    return transferSyntax;
 }
 
 function loadImage (imageId, options) {
-  const start = new Date().getTime();
-  const uri = imageId.substring(7);
+    const start = new Date().getTime();
+    const uri = imageId.substring(7);
 
-  const promise = new Promise((resolve, reject) => {
-    // check to make sure we have metadata for this imageId
-    const metaData = metaDataManager.get(imageId);
+    const promise = new Promise((resolve, reject) => {
+        // check to make sure we have metadata for this imageId
+        const metaData = metaDataManager.get(imageId);
 
-    if (metaData === undefined) {
-      const error = new Error(`no metadata for imageId ${imageId}`);
+        if (metaData === undefined) {
+            const error = new Error(`no metadata for imageId ${imageId}`);
 
-      return reject(error);
-    }
+            return reject(error);
+        }
 
-    // TODO: load bulk data items that we might need
-    const mediaType = 'multipart/related; type="application/octet-stream"'; // 'image/dicom+jp2';
+        // TODO: load bulk data items that we might need
+        let mediaType = 'multipart/related; type="application/octet-stream"'; // 'image/dicom+jp2';
+        //根据TransferSyntax决定mediaType
+        let tsuid;
+        if (metaData['00020010']
+            && ("1.2.840.10008.1.2.4.90" === metaData['00020010'].Value[0] || "1.2.840.10008.1.2.4.91" === metaData['00020010'].Value[0])
+        ) {
+            tsuid = metaData['00020010'].Value[0];
+            mediaType = 'multipart/related; type="image/jp2"';
+        } else if (
+            metaData['00020010'] &&
+            (
+                "1.2.840.10008.1.2.4.50" === metaData['00020010'].Value[0] ||
+                "1.2.840.10008.1.2.4.51" === metaData['00020010'].Value[0] ||
+                "1.2.840.10008.1.2.4.70" === metaData['00020010'].Value[0] ||
+                "1.2.840.10008.1.2.4.80" === metaData['00020010'].Value[0] ||
+                "1.2.840.10008.1.2.4.81" === metaData['00020010'].Value[0]
+            )
+        ) {
+            tsuid = metaData['00020010'].Value[0];
+            mediaType = 'multipart/related; type="image/jpeg"';
+        }
 
-    // get the pixel data from the server
-    getPixelData(uri, imageId, mediaType).then((result) => {
-      const transferSyntax = getTransferSyntaxForContentType(result.contentType);
-      const pixelData = result.imageFrame.pixelData;
-      const imagePromise = createImage(imageId, pixelData, transferSyntax, options);
+        // get the pixel data from the server
+        getPixelData(uri, imageId, mediaType).then((result) => {
+            const transferSyntax = tsuid || getTransferSyntaxForContentType(result.contentType);
+            const pixelData = result.imageFrame.pixelData;
+            const imagePromise = createImage(imageId, pixelData, transferSyntax, options);
 
-      imagePromise.then((image) => {
-        // add the loadTimeInMS property
-        const end = new Date().getTime();
+            imagePromise.then((image) => {
+                // add the loadTimeInMS property
+                const end = new Date().getTime();
 
-        image.loadTimeInMS = end - start;
-        resolve(image);
-      }, reject);
-    }, reject);
-  });
+                image.loadTimeInMS = end - start;
+                resolve(image);
+            }, reject);
+        }, reject);
+    });
 
-  return {
-    promise,
-    cancelFn: undefined
-  };
+    return {
+        promise,
+        cancelFn: undefined
+    };
 }
 
 export default loadImage;
